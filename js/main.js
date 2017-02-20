2ce92d42f9f24e5abbb3290a701144102c4cce91
Marija Jovicevic <marija.jovicevic@diwanee.com>
2017-02-20 16:53:48 +0100

    fixes and addings
    align project with real one

--- js/main.js
+++ js/main.js
@@ -1,67 +1,88 @@
 //navigation
 $(document).ready(function () {
-$( ".cross" ).hide();
-$( ".hamburger" ).show(); //to be safe that it will show
-$( ".b-header__menuwrapper" ).hide();
-$( ".b-header__socials" ).hide();
+	//Navigation
+	// on click open
+	$('.hamburger').click(function () {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+		$( "body" ).addClass('body-menu-open');
+		$( ".b-header" ).addClass('menu-open');
+		$('.b-header__menuwrapper').slideToggle('slow');
+	});
+	// on click close
+	$('.cross').click(function () {
+		$( ".hamburger" ).show();
+		$( ".cross" ).hide();
+		$( "body" ).removeClass('body-menu-open');
+		$( ".b-header" ).removeClass('menu-open');
+		$('.b-header__menuwrapper').slideToggle('slow');
+	});
 
 
-//on click open
-$( ".hamburger" ).click(function() {
-  $( ".b-header__menuwrapper" ).slideToggle( "slow", function() {
-    $( ".b-header__socials" ).show();
-$( ".b-header" ).addClass('open');
-    $( ".hamburger" ).hide();
-    $( ".cross" ).show();
+
+	//end navigation
+
+	//load more
+	$( ".c-btn-loadmore" ).click(function() {
+		$( "#loadmore" ).slideToggle( "slow", function() {
+		});
+	});
 
 
-  });
+
+
+	//equal heights
+	/* Thanks to CSS Tricks for pointing out this bit of jQuery
+	http://css-tricks.com/equal-height-blocks-in-rows/
+	It's been modified into a function called at page load and then each time the page is resized. One large modification was to remove the set height before each new calculation. */
+
+	equalheight = function(container){
+
+		var currentTallest = 0,
+		currentRowStart = 0,
+		rowDivs = new Array(),
+		$el,
+		topPosition = 0;
+		$(container).each(function() {
+
+			$el = $(this);
+			$($el).height('auto')
+			topPostion = $el.position().top;
+
+			if (currentRowStart != topPostion) {
+				for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+					rowDivs[currentDiv].height(currentTallest);
+				}
+				rowDivs.length = 0; // empty the array
+				currentRowStart = topPostion;
+				currentTallest = $el.height();
+				rowDivs.push($el);
+			} else {
+				rowDivs.push($el);
+				currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+			}
+			for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+				rowDivs[currentDiv].height(currentTallest);
+			}
+		});
+	}
+
+	$(window).load(function() {
+		equalheight('.b-list .article .article-title');
+	});
+
+	$(window).resize(function(){
+		equalheight('.b-list .article .article-title');
+	});
+
+	////reload page so JS menu can work
+	$(window).bind('resize', function(e)
+	{
+		if (window.RT) clearTimeout(window.RT);
+		window.RT = setTimeout(function()
+		{
+			this.location.reload(false); // false to get page from cache
+		}, 100);
+	});
+
 });
-
-//on click close
-$( ".cross" ).click(function() {
-  $( ".b-header__menuwrapper" ).slideToggle( "slow", function() {
-    $( ".b-header__socials" ).hide();
-    $( ".b-header" ).removeClass('open');
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
-
-  });
-});
-
-//menu bp handler
-var menuCalc = function() {
-  var w = $(this).width();
-  if(w > 1024) {
-         $( ".b-header__socials" ).css({display:'inline-block'});
-    $( ".b-header__menutriggers" ).hide();
-    $( ".b-header__menuwrapper" ).css({display:'block', height:'auto', bottom:'auto', left:'auto', right:'auto', top:'1.1rem' });
-
-  }
-  else {
-    $( ".b-header__menutriggers" ).show();
-
-    // $( ".b-header__menu--list" ).css({display:'none'});
-    $('.b-header__menu--list').height($(window).height());
-
-  }
-}
-
-//call bp handler
-$(window).resize(function(){
-  menuCalc();
-});
-menuCalc();
-});
-
-//end navigation
-
-//load more
-$(document).ready(function () {
-  $( ".loadmoreDiv" ).addClass('hide');
-    $( ".c-btn-loadmore" ).click(function() {
-      $( ".loadmoreDiv" ).slideToggle( "slow", function() {
-  $( ".loadmoreDiv" ).removeClass('hide');
-      });
-    });
-    });
